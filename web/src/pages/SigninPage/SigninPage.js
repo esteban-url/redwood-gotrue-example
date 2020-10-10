5d98c2cc4fa1d2d2977e187410ab5410e58365b3
Esteban Rojas <estebanjrojas@gmail.com>
2020-10-10 12:34:34 -0600

    updating the signin page

--- web/src/pages/SigninPage/SigninPage.js
+++ web/src/pages/SigninPage/SigninPage.js
@@ -1,6 +1,13 @@
-import { Form, TextField, PasswordField, Submit } from '@redwoodjs/forms'
+import {
+  Form,
+  TextField,
+  PasswordField,
+  Submit,
+  CheckboxField,
+  Label,
+} from '@redwoodjs/forms'
 import { useAuth } from '@redwoodjs/auth'
-import { navigate, routes } from '@redwoodjs/router'
+import { Link, navigate, routes } from '@redwoodjs/router'
 
 import GlobalLayout from 'src/layouts/GlobalLayout/GlobalLayout'
 
@@ -10,7 +17,11 @@ const SigninPage = () => {
 
   const onSubmit = (data) => {
     setError(null)
-    logIn({ email: data.email, password: data.password, remember: true })
+    logIn({
+      email: data.email,
+      password: data.password,
+      remember: data.remember,
+    })
       .then(() => navigate(routes.members()))
       .catch((error) => setError(error.message))
   }
@@ -22,8 +33,13 @@ const SigninPage = () => {
         {error ? <p>{error}</p> : null}
         <TextField name="email" placeholder="email" />
         <PasswordField name="password" placeholder="password" />
+        <CheckboxField name="remeber" />
+        <Label name="remeber">Remember me</Label>
         <Submit>Sign In</Submit>
       </Form>
+      <p>
+        Not a member? <Link to={routes.signup()}>Sign up</Link> now!
+      </p>
     </GlobalLayout>
   )
 }
