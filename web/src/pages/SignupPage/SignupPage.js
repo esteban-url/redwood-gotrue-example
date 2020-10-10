fa8d8af4cafda9b5c73ae287c21150a24d643bd2
Esteban Rojas <estebanjrojas@gmail.com>
2020-10-10 12:36:33 -0600

    updating the signup page

--- web/src/pages/SignupPage/SignupPage.js
+++ web/src/pages/SignupPage/SignupPage.js
@@ -2,7 +2,7 @@ import { Form, TextField, PasswordField, Submit } from '@redwoodjs/forms'
 import { useAuth } from '@redwoodjs/auth'
 
 import GlobalLayout from 'src/layouts/GlobalLayout/GlobalLayout'
-import { navigate, routes } from '@redwoodjs/router'
+import { Link, navigate, routes } from '@redwoodjs/router'
 
 const SignupPage = () => {
   const { client } = useAuth()
@@ -18,12 +18,17 @@ const SignupPage = () => {
   return (
     <GlobalLayout>
       <h1>Sign Up</h1>
-      <Form onSubmit={onSubmit}>
-        {error ? <p>{error}</p> : null}
-        <TextField name="email" placeholder="email" />
-        <PasswordField name="password" placeholder="password" />
-        <Submit>Sign Up</Submit>
-      </Form>
+        <>
+          <Form onSubmit={onSubmit}>
+            {error ? <p>{error}</p> : null}
+            <TextField name="email" placeholder="email" />
+            <PasswordField name="password" placeholder="password" />
+            <Submit>Sign Up</Submit>
+          </Form>
+          <p>
+            Already a member? <Link to={routes.signin()}>Sign in</Link> instead
+          </p>
+        </>
     </GlobalLayout>
   )
 }
